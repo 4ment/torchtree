def8ddee7694d63fb9f87fd36fd4c89e9d7a2190
Mathieu Fourment <m.fourment@gmail.com>
2020-02-03 05:27:28 +1100

    Add skygrid

--- phylotorch/coalescent.py
+++ phylotorch/coalescent.py
@@ -15,10 +15,9 @@ class ConstantCoalescent(object):
         heights_sorted = torch.gather(heights, 0, indices)
         node_mask_sorted = torch.gather(node_mask, 0, indices)
         lineage_count = torch.where(node_mask_sorted, torch.tensor([-1]), torch.tensor([1])).cumsum(0)[:-1]
-        temp = node_mask_sorted[1:] | node_mask_sorted[:-1]
-        durations = torch.masked_select(heights_sorted[1:] - heights_sorted[:-1], temp)
-        lchoose2 = torch.masked_select(lineage_count * (lineage_count - 1) / 2.0, temp)
-        return torch.sum(-lchoose2 * durations / theta - torch.log(theta).repeat(self.taxon_count - 1))
+        durations = heights_sorted[1:] - heights_sorted[:-1]
+        lchoose2 = lineage_count * (lineage_count - 1) / 2.0
+        return torch.sum(-lchoose2 * durations / theta) - (self.taxon_count - 1)*torch.log(theta)
 
 
 class PiecewiseConstantCoalescent(ConstantCoalescent):
@@ -38,7 +37,32 @@ class PiecewiseConstantCoalescent(ConstantCoalescent):
         masks = node_mask_sorted[1:]
         lchoose2 = lineage_count * (lineage_count - 1) / 2.0
 
-        thetas = torch.cat([torch.full((self.taxon_count - 1,), 0.0), theta], 0)
+        thetas = torch.cat([torch.full((self.taxon_count - 1,), 0.0, dtype=torch.float64), theta], 0)
 
         thetas_masked = torch.masked_select(thetas, masks)
         return torch.sum(-torch.masked_select(lchoose2 * durations, masks) / thetas_masked - torch.log(thetas_masked))
+
+
+class PiecewiseConstantCoalescentGrid(ConstantCoalescent):
+    def __init__(self, sampling_times, grid):
+        super(PiecewiseConstantCoalescentGrid, self).__init__(sampling_times)
+        self.grid = grid
+
+    def log_prob(self, thetas, node_heights):
+        heights = torch.cat([self.sampling_times, node_heights, self.grid], 0)
+        node_mask = torch.cat([torch.full((self.taxon_count,), 1),  # sampling event
+                              torch.full((self.taxon_count - 1,), -1),  # coalescent event
+                              torch.full((self.grid.shape[0],), 0)], 0)  # no event
+
+        indices = torch.argsort(heights, descending=False)
+        heights_sorted = torch.gather(heights, 0, indices)
+        node_mask_sorted = torch.gather(node_mask, 0, indices)
+        lineage_count = node_mask_sorted.cumsum(0)[:-1]
+
+        durations = heights_sorted[1:] - heights_sorted[:-1]
+        lchoose2 = lineage_count * (lineage_count - 1) / 2.0
+
+        thetas_indices = torch.where(node_mask_sorted == 0, torch.tensor([1], dtype=torch.long),
+                               torch.tensor([0], dtype=torch.long)).cumsum(0)
+        thetas_masked = torch.masked_select(thetas[thetas_indices], node_mask_sorted == -1)
+        return -torch.sum(lchoose2 * durations / thetas[thetas_indices[:-1]]) - torch.log(thetas_masked).sum()
