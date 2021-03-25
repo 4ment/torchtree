0b696c5d502cce1fb5bc3d951c9dbcee64fdfe1e
Mathieu Fourment <m.fourment@gmail.com>
2021-03-25 17:16:32 +1100

    Clean test_tree_likelihood and add test_taxa.py

---
+++ test/test_taxa.py
@@ -0,0 +1,30 @@
+from phylotorch.evolution.taxa import Taxa
+
+
+def test_taxa():
+    taxa = {
+        "id": "taxa",
+        "type": "phylotorch.evolution.taxa.Taxa",
+        "taxa": [
+            {
+                "id": "A_Belgium_2_1981",
+                "type": "phylotorch.evolution.taxa.Taxon",
+                "attributes": {
+                    "date": 1981
+                }
+            },
+            {
+                "id": "A_ChristHospital_231_1982",
+                "type": "phylotorch.evolution.taxa.Taxon",
+                "attributes": {
+                    "date": 1982
+                }
+            }
+            ]
+    }
+    dic = {}
+    taxa = Taxa.from_json(taxa, dic)
+    assert len(taxa) == 2
+    assert taxa[1].id == 'A_ChristHospital_231_1982'
+    assert taxa[1]['date'] == 1982
+
