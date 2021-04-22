abbec769bff7b9d89c2c03a2ff986e4f16b56eed
Mathieu Fourment <m.fourment@gmail.com>
2021-04-23 07:39:55 +1000

    Improved optim.py: - parsing of scheduler - param group for torch.optim.Optimizer - type hint - checkpointing

--- phylotorch/optim.py
+++ phylotorch/optim.py
@@ -1,47 +1,70 @@
 import abc
+import copy
 import inspect
+import json
+import os
+from typing import Dict, Union, Tuple
 
 import torch
+from torch.optim import Optimizer as TorchOptimizer
+from torch.optim.lr_scheduler import _LRScheduler as TorchScheduler
 
-from .core.model import Parameter
+from .core.model import Parameter, Parametric, CallableModel
+from .core.parameter_encoder import ParameterEncoder
 from .core.runnable import Runnable
 from .core.serializable import JSONSerializable
-from .core.utils import get_class, process_objects, SignalHandler, validate
+from .core.utils import get_class, process_objects, SignalHandler, JSONParseError
+from .typing import ListParameter, ListTensor
 
 
 class Convergence(JSONSerializable):
 
     @abc.abstractmethod
-    def check(self, iterations):
+    def check(self, iteration: int, *args, **kwargs) -> bool:
         pass
 
 
 class StanConvergence(Convergence):
 
-    def __init__(self, loss, every, samples):
+    def __init__(self, loss: CallableModel, every: int, samples: int, start: int = 0):
         self.loss = loss
         self.every = every
         self.samples = samples
+        self.start = start
 
-    def check(self, iterations):
-        if iterations % self.every == 0:
-            with torch.no_grad():
-                elbo = self.loss(samples=self.samples)
+    def check(self, iteration: int, *args, **kwargs) -> bool:
+        if iteration >= self.start and iteration % self.every == 0:
+            if self.samples == 0:
+                elbo = self.loss.lp
+            else:
+                with torch.no_grad():
+                    elbo = self.loss(samples=self.samples)
             # TODO: implement
-            print(iterations, 'ELBO', elbo)
+            print(iteration, 'ELBO', elbo)
         return True
 
     @classmethod
-    def from_json(cls, data, dic):
+    def from_json(cls, data: Dict[str, any], dic: Dict[str, any]) -> 'StanConvergence':
         loss = process_objects(data['loss'], dic)
         every = data.get('every', 100)
         samples = data.get('samples', 100)
-        return cls(loss, every, samples)
+        start = data.get('start', 0)
+        return cls(loss, every, samples, start)
 
 
 class Optimizer(JSONSerializable, Runnable):
+    """
+    A wrapper for torch.optim.Optimizer objects.
 
-    def __init__(self, parameters, loss, optimizer, iterations, **kwargs):
+    :param parameters: list of Parameter
+    :param loss: loss function
+    :param optimizer: a torch.optim.Optimizer object
+    :param iterations: number of iterations
+    :param kwargs:
+    """
+
+    def __init__(self, parameters: ListParameter, loss: CallableModel, optimizer: TorchOptimizer, iterations: int,
+                 **kwargs):
         self.parameters = parameters
         self.loss = loss
         self.optimizer = optimizer
@@ -51,7 +74,7 @@ class Optimizer(JSONSerializable, Runnable):
         self.loggers = kwargs.get('loggers', ())
         self.maximize = kwargs.get('maximize', True)
 
-    def run(self):
+    def run(self) -> None:
         for logger in self.loggers:
             if hasattr(logger, 'init'):
                 logger.init()
@@ -63,7 +86,7 @@ class Optimizer(JSONSerializable, Runnable):
         for p in self.parameters:
             p.requires_grad = True
 
-        for epoch in range(1, self.iterations):
+        for epoch in range(1, self.iterations + 1):
             if handler.stop:
                 break
 
@@ -88,24 +111,60 @@ class Optimizer(JSONSerializable, Runnable):
                 if not res:
                     break
 
+            if epoch % 1000 == 0:
+                if not os.path.lexists('checkpoint.json'):
+                    # for var_name in self.optimizer.state_dict():
+                    #     print(var_name, "\t", self.optimizer.state_dict()[var_name])
+                    # torch.save(self.optimizer.state_dict(), 'checkpoint.json')
+                    with open('checkpoint.json', 'w') as fp:
+                        json.dump(self.parameters, fp, cls=ParameterEncoder, indent=2)
+                else:
+                    # torch.save(self.optimizer.state_dict(), 'checkpoint-new.json')
+                    with open('checkpoint-new.json', 'w') as fp:
+                        json.dump(self.parameters, fp, cls=ParameterEncoder, indent=2)
+                    os.rename('checkpoint.json', 'checkpoint-old.json')
+                    os.rename('checkpoint-new.json', 'checkpoint.json')
+                    os.remove('checkpoint-old.json')
+
         for logger in self.loggers:
             if hasattr(logger, 'finalize'):
                 logger.finalize()
 
+    @staticmethod
+    def parse_params(params: Dict[str, Union[list, float]], dic) -> Tuple[ListTensor, ListParameter]:
+        parameter_or_parametric_list = process_objects(params, dic)
+        if not isinstance(parameter_or_parametric_list, list):
+            parameter_or_parametric_list = [parameter_or_parametric_list]
+
+        tensors = []
+        parameters = []
+        for poml in parameter_or_parametric_list:
+            if isinstance(poml, Parameter):
+                tensors.append(poml.tensor)
+                parameters.append(poml)
+            elif isinstance(poml, Parametric):
+                for parameter in poml.parameters():
+                    tensors.append(parameter.tensor)
+                    parameters.append(parameter)
+            else:
+                raise JSONParseError(
+                    'Optimizable expects a list of Parameters or Parametric models\n{} was provided'.format(type(poml)))
+        return tensors, parameters
+
     @classmethod
-    def from_json(cls, data, dic):
+    def from_json(cls, data: Dict[str, any], dic: Dict[str, any]) -> 'Optimizer':
         rules = {
             'loss': {'type': 'object|string', 'instanceof': 'CallableModel'},
             'parameters': {'type': 'object|string', 'instanceof': 'Parameter', 'list': True},
             'iterations': {'type': 'int', 'constraint': {'>': 0}},
             'algorithm': {'type': 'string', 'instanceof': 'torch.optim.Optimizer'},
             'maximize': {'type': 'bool', 'optional': True},
-            'loggers': {'type': 'object', 'instanceof': 'Scheduler', 'list': True, 'optional': True},
+            'loggers': {'type': 'object', 'instanceof': 'Logger', 'list': True, 'optional': True},
             'scheduler': {'type': 'object', 'instanceof': 'Scheduler', 'optional': True},
             'lr': {'type': 'numbers.Number', 'constraint': {'>': 0.0}, 'optional': True},
             'convergence': {'type': 'object', 'optional': True}
         }
-        validate(data, rules)
+        # validate(data, rules)
 
         iterations = data['iterations']
 
@@ -117,31 +176,38 @@ class Optimizer(JSONSerializable, Runnable):
                 loggers = list(loggers)
             optionals['loggers'] = loggers
 
-        # if 'loggers' in data:
-        #     loggers = [process_object(logger, dic) for logger in data["loggers"]]
-        # loggers = list(map(Logger.from_json, data["loggers"]))
-
         loss = process_objects(data['loss'], dic)
-        parameters = process_objects(data['parameters'], dic)
+
+        optimizer_params = []  # an iterable of torch.Tensor s or dict s. Specifies what Tensors should be optimized
+        parameters = []  # a list of phylotorch.Parameters
+        if isinstance(data['parameters'][0], dict):
+            # [{'params': ['model']},
+            #  {'params': ["parameter1", "parameter2"], 'lr': 1e-3}
+            # ], lr=1e-2, momentum=0.9
+            for param_groups in data['parameters']:
+                t, p = cls.parse_params(param_groups['params'], dic)
+                param_groups_cpy = copy.deepcopy(param_groups)
+                param_groups_cpy['params'] = t
+                optimizer_params.append(param_groups_cpy)
+                parameters.extend(p)
+
+        else:
+            # "parameters": ["model", "parameter1", "parameter2"]
+            optimizer_params, parameters = cls.parse_params(data['parameters'], dic)
 
         # instanciate torch.optim.optimizer
         optim_class = get_class(data['algorithm'])
-        tensors = [p.tensor for p in parameters]
         signature_params = list(inspect.signature(optim_class.__init__).parameters)
         kwargs = {}
         for arg in signature_params[1:]:
             if arg in data:
                 kwargs[arg] = data[arg]
-        optimizer = optim_class(tensors, **kwargs)
+        optimizer = optim_class(optimizer_params, **kwargs)  # type: int
 
         # instanciate torch.optim.scheduler
         if 'scheduler' in data:
             klass = get_class(data['scheduler']['type'])
-            kwargs = {}
-            if 'lr_lambda' in data['scheduler']:
-                kwargs['lr_lambda'] = eval(data['scheduler']['lr_lambda'])
-            scheduler = klass(optimizer, **kwargs)
-            optionals['scheduler'] = scheduler
+            optionals['scheduler'] = klass.from_json(data['scheduler'], None, optimizer=optimizer)
 
         # instanciate phylotorch.optim.Convergence
         if 'convergence' in data:
@@ -153,19 +219,28 @@ class Optimizer(JSONSerializable, Runnable):
 
 
 class Scheduler(JSONSerializable):
+    """
+    A wrapper for :class:`~torch.optim.lr_scheduler` objects.
+
+    :param scheduler: a :class:`~torch.optim.lr_scheduler`
+    """
+
+    def __init__(self, scheduler: TorchScheduler):
+        self.scheduler = scheduler
+
+    def step(self, *args) -> None:
+        self.scheduler.step(*args)
 
     @classmethod
-    def from_json(cls, data, dic):
-        klass = get_class(data['algorithm'])
-
-        signature_params = inspect.signature(klass.__init__).parameters
-        params = []
-        params.append(data['optimizer'])
-        # lr_lambda = lambda epoch: 1.0 / np.sqrt(epoch + 1)
-        if 'lr_lambda' in data:
-            params.append(eval(data['lr_lambda']))
-        # for arg in signature_params[2:]:
-        #     if arg in data:
-        #         params.append(eval(data[arg]))
-
-        return klass(*params)
+    def from_json(cls, data: Dict[str, any], dic: Dict[str, any], **kwargs) -> 'Scheduler':
+        scheduler_class = get_class(data['scheduler'])
+        signature_params = list(inspect.signature(scheduler_class.__init__).parameters)
+        optionals = {}
+        for arg in signature_params[1:]:
+            if arg in data:
+                if arg == 'lr_lambda':
+                    optionals['lr_lambda'] = eval(data['lr_lambda'])
+                else:
+                    optionals[arg] = data[arg]
+        scheduler = scheduler_class(kwargs['optimizer'], **optionals)
+        return cls(scheduler)
---
+++ phylotorch/typing.py
@@ -0,0 +1,8 @@
+from typing import List
+
+import torch
+
+from phylotorch import Parameter
+
+ListTensor = List[torch.tensor]
+ListParameter = List[Parameter]
