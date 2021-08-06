59399b74b4b1b64125a63d2d772fc2e8edcab21b
Mathieu Fourment <m.fourment@gmail.com>
2021-08-06 13:15:36 +1000

    Adopt PEP 518

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
-from pathlib import Path
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-setup(
-    name='phylotorch',
-    version='1.0.0',
-    packages=find_packages(),
-    url='https://github.com/4ment/phylotorch',
-    author='Mathieu Fourment',
-    author_email='mathieu.fourment@uts.edu.au',
-    keywords="phylogenetics, variational, Bayes, pytorch",
-    description='Phylogenetic inference with pytorch',
-    license='GPL3',
-    python_requires=">=3.6",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-    ],
-    install_requires=[
-        line.strip()
-        for line in Path('requirements.txt').read_text('utf-8').splitlines()
-    ],
-    extras_require={"libsbn": ["libsbn"], "test": ['pytest']},
-    entry_points={'console_scripts': ['phylotorch=phylotorch.phylotorch:main']},
-)
+if __name__ == '__main__':
+    setup()
