364f1b8dbd0fe74fe16a99b538923c4f71ff46f8
Mathieu Fourment <m.fourment@gmail.com>
2021-08-10 14:53:02 +1000

    Implement tree likelihood rescaling
     - Add a test for rescaling
    
     - Use isort, black and flake8

--- phylotorch/evolution/tree_likelihood.py
+++ phylotorch/evolution/tree_likelihood.py
@@ -3,67 +3,116 @@ from typing import Optional
 import torch
 import torch.distributions
 
+from ..core.model import CallableModel
+from ..core.utils import process_object
 from .branch_model import BranchModel
 from .site_model import SiteModel
 from .site_pattern import SitePattern
 from .substitution_model import SubstitutionModel
 from .tree_model import TreeModel
-from ..core.model import CallableModel
-from ..core.utils import process_object
 
 
-def calculate_treelikelihood(partials, weights, post_indexing, mats, freqs):
+def calculate_treelikelihood(
+    partials: list,
+    weights: torch.Tensor,
+    post_indexing: list,
+    mats: torch.Tensor,
+    freqs: torch.Tensor,
+) -> torch.Tensor:
+    """Simple function for calculating the log tree likelihood.
+
+    :param partials: list of tensors of partials [S,N] leaves and [S,N] internals
+    :param weights: [N]
+    :param post_indexing: list of indexes in postorder
+    :param mats: tensor of probability matrices [B,S,S]
+    :param freqs: tensor of frequencies [S]
+    :return: tree log likelihood [1]
+    """
     for node, left, right in post_indexing:
-        partials[node] = torch.matmul(mats[left], partials[left]) * torch.matmul(mats[right], partials[right])
-    return torch.sum(torch.log(torch.matmul(freqs, partials[post_indexing[-1][0]])) * weights)
+        partials[node] = torch.matmul(mats[left], partials[left]) * torch.matmul(
+            mats[right], partials[right]
+        )
+    return torch.sum(
+        torch.log(torch.matmul(freqs, partials[post_indexing[-1][0]])) * weights
+    )
 
 
-def calculate_treelikelihood_discrete(partials, weights, post_indexing, mats, freqs, props):
-    r""" Calculate log likelihood
+def calculate_treelikelihood_discrete(
+    partials: list,
+    weights: torch.Tensor,
+    post_indexing: list,
+    mats: torch.Tensor,
+    freqs: torch.Tensor,
+    props: torch.Tensor,
+) -> torch.Tensor:
+    r"""Calculate log tree likelihood
 
     :param partials: list of tensors of partials [S,N] leaves and [...,K,S,N] internals
     :param weights: [N]
     :param post_indexing: list of indexes in postorder
     :param mats: tensor of probability matrices [...,B,K,S,S]
-    :param freqs: tensor of frequencies [...,S]
+    :param freqs: tensor of frequencies [...,1,S]
     :param props: tensor of proportions [...,K,1,1]
-    :return:
+    :return: tree log likelihood [batch]
     """
     for node, left, right in post_indexing:
-        partials[node] = (mats[..., left, :, :, :] @ partials[left]) * (mats[..., right, :, :, :] @ partials[right])
-    return torch.sum(torch.log(freqs @ torch.sum(props * partials[post_indexing[-1][0]], -3)) * weights, -1)
+        partials[node] = (mats[..., left, :, :, :] @ partials[left]) * (
+            mats[..., right, :, :, :] @ partials[right]
+        )
+    return torch.sum(
+        torch.log(freqs @ torch.sum(props * partials[post_indexing[-1][0]], -3))
+        * weights,
+        -1,
+    )
 
 
-def calculate_treelikelihood_discrete_rescaled(partials, weights, post_indexing, mats, freqs, props):
-    # TODO: adapt to calculate using batch samples
-    r""" Calculate log likelihood
+def calculate_treelikelihood_discrete_rescaled(
+    partials: list,
+    weights: torch.Tensor,
+    post_indexing: list,
+    mats: torch.Tensor,
+    freqs: torch.Tensor,
+    props: torch.Tensor,
+) -> torch.Tensor:
+    r"""Calculate log tree likelihood using rescaling
 
-    :param partials: list of tensors of partials [S,N] leaves and [K,S,N] internals
+    :param partials: list of tensors of partials [S,N] leaves and [...,K,S,N] internals
     :param weights: [N]
     :param post_indexing:
-    :param mats: tensor of matrices [B,K,S,S]
-    :param freqs: tensor of frequencies [S]
-    :param props: tensor of proportions [K,1,1]
-    :return:
+    :param mats: tensor of matrices [...,B,K,S,S]
+    :param freqs: tensor of frequencies [...,1,S]
+    :param props: tensor of proportions [...,K,1,1]
+    :return: tree log likelihood [batch]
     """
     scalers = []
     for node, left, right in post_indexing:
-        partials[node] = torch.matmul(mats[left], partials[left]) * torch.matmul(
-            mats[right], partials[right])
-        scaler = torch.max(partials[node])
-        partials[node] = partials[node].clone() / scaler
-        scalers.append(torch.unsqueeze(scaler, 0))
+        partial = (mats[..., left, :, :, :] @ partials[left]) * (
+            mats[..., right, :, :, :] @ partials[right]
+        )
+        scaler, _ = torch.max(partial, -2)
+        scalers.append(scaler)
+        partials[node] = partial / scaler
     return torch.sum(
-        torch.log(torch.matmul(freqs, torch.sum(props * partials[post_indexing[-1][0]], 0))) * weights) + torch.sum(
-        torch.cat(scalers).log())
+        (
+            torch.log(freqs @ torch.sum(props * partials[post_indexing[-1][0]], dim=-3))
+            + torch.sum(torch.cat(scalers, -2).log(), dim=-2)
+        )
+        * weights,
+        dim=-1,
+    )
 
 
 class TreeLikelihoodModel(CallableModel):
-
-    def __init__(self, id_: Optional[str], site_pattern: SitePattern, tree_model: TreeModel,
-                 subst_model: SubstitutionModel,
-                 site_model: SiteModel, clock_model: BranchModel = None):
-        super(TreeLikelihoodModel, self).__init__(id_)
+    def __init__(
+        self,
+        id_: Optional[str],
+        site_pattern: SitePattern,
+        tree_model: TreeModel,
+        subst_model: SubstitutionModel,
+        site_model: SiteModel,
+        clock_model: BranchModel = None,
+    ):
+        super().__init__(id_)
         self.site_pattern = site_pattern
         self.tree_model = tree_model
         self.subst_model = subst_model
@@ -74,32 +123,11 @@ class TreeLikelihoodModel(CallableModel):
         self.add_model(site_model)
         if clock_model:
             self.add_model(clock_model)
+        self.rescale = False
 
-    def log_prob(self, value):
-        self.site_model.update(value)
-        rates = self.site_model.rates().reshape(1, -1)
-        probs = self.site_model.probabilities().reshape((-1, 1, 1))
-
-        self.tree_model.update(value)
-        if self.clock_model is None:
-            bls = torch.unsqueeze(
-                torch.cat((self.tree_model.branch_lengths(), torch.zeros(1, dtype=torch.float64))).reshape(-1,
-                                                                                                           1) * rates,
-                -1)
-        else:
-            self.clock_model.update(value)
-            clock_rates = self.clock_model.rates().reshape(-1, 1)
-            bls = torch.unsqueeze(clock_rates * self.tree_model.branch_lengths().reshape(-1, 1) * rates, -1)
-        self.subst_model.update(value)
-        mats = self.subst_model.p_t(bls)
-
-        return calculate_treelikelihood_discrete(self.site_pattern.partials, self.site_pattern.weights,
-                                                 self.tree_model.postorder, mats,
-                                                 self.subst_model.frequencies, probs)
-
-    def _call(self):
+    def _call(self, *args, **kwargs) -> torch.Tensor:
         branch_lengths = self.tree_model.branch_lengths()
-        batch_shape = branch_lengths.shape[:-1]
+        batch_shape = self.site_model.rates().shape[:-1]
         rates = self.site_model.rates()
         # for models like JC69 rates is always tensor([1.0])  (i.e. batch_shape == [])
         if rates.dim() == 1:
@@ -109,36 +137,58 @@ class TreeLikelihoodModel(CallableModel):
         probs = self.site_model.probabilities().reshape((-1, 1, 1))
 
         if self.clock_model is None:
-            bls = torch.cat((branch_lengths, torch.zeros(batch_shape + (1,), dtype=branch_lengths.dtype)), -1)
+            bls = torch.cat(
+                (
+                    branch_lengths,
+                    torch.zeros(batch_shape + (1,), dtype=branch_lengths.dtype),
+                ),
+                -1,
+            )
         else:
-            clock_rates = self.clock_model.rates
-            bls = clock_rates * branch_lengths
+            if branch_lengths.dim() == 1:
+                bls = self.clock_model.rates * branch_lengths.expand(
+                    batch_shape + (1, -1)
+                )
+            else:
+                bls = self.clock_model.rates * branch_lengths
 
         mats = self.subst_model.p_t(bls.reshape(batch_shape + (-1, 1)) * rates)
         frequencies = self.subst_model.frequencies
-
-        return calculate_treelikelihood_discrete(self.site_pattern.partials,
-                                                 self.site_pattern.weights,
-                                                 self.tree_model.postorder,
-                                                 mats,
-                                                 frequencies.reshape(frequencies.shape[:-1] + (1, -1)),
-                                                 probs)
+        if self.rescale:
+            log_p = calculate_treelikelihood_discrete_rescaled(
+                self.site_pattern.partials,
+                self.site_pattern.weights,
+                self.tree_model.postorder,
+                mats,
+                frequencies.reshape(frequencies.shape[:-1] + (1, -1)),
+                probs,
+            )
+        else:
+            log_p = calculate_treelikelihood_discrete(
+                self.site_pattern.partials,
+                self.site_pattern.weights,
+                self.tree_model.postorder,
+                mats,
+                frequencies.reshape(frequencies.shape[:-1] + (1, -1)),
+                probs,
+            )
+        return log_p
 
     def update(self, value):
         pass
 
     def handle_model_changed(self, model, obj, index):
-        pass
+        self.fire_model_changed()
 
     def handle_parameter_changed(self, variable, index, event):
         pass
 
     @property
-    def batch_shape(self):
+    def batch_shape(self) -> torch.Size:
         return self.tree_model.batch_shape
 
     @property
-    def sample_shape(self):
+    def sample_shape(self) -> torch.Size:
         return self.tree_model.sample_shape
 
     @classmethod
--- test/test_tree_likelihood.py
+++ test/test_tree_likelihood.py
@@ -1,28 +1,65 @@
-import numpy as np
 import torch
 
 import phylotorch.evolution.tree_likelihood as likelihood
 from phylotorch import Parameter
 from phylotorch.evolution.branch_model import StrictClockModel
 from phylotorch.evolution.site_model import WeibullSiteModel
-from phylotorch.evolution.site_pattern import get_dna_leaves_partials_compressed, SitePattern
+from phylotorch.evolution.site_pattern import (
+    SitePattern,
+    get_dna_leaves_partials_compressed,
+)
 from phylotorch.evolution.substitution_model import JC69
 from phylotorch.evolution.tree_model import TimeTreeModel
 from phylotorch.io import read_tree_and_alignment
 
 
-def test_calculate_pytorch(tiny_newick_file, tiny_fasta_file, jc69_model):
+def _prepare_tiny(tiny_newick_file, tiny_fasta_file):
     tree, dna = read_tree_and_alignment(tiny_newick_file, tiny_fasta_file, False, False)
-    branch_lengths = np.array(
-        [float(node.edge_length) for node in sorted(list(tree.postorder_node_iter())[:-1], key=lambda x: x.index)])
+    branch_lengths = torch.tensor(
+        [
+            float(node.edge_length)
+            for node in sorted(
+                list(tree.postorder_node_iter())[:-1], key=lambda x: x.index
+            )
+        ],
+        dtype=torch.float64,
+    )
     indices = []
     for node in tree.postorder_internal_node_iter():
         indices.append([node.index] + [child.index for child in node.child_nodes()])
-    branch_lengths_tensor = torch.tensor(branch_lengths, requires_grad=True)
+
     partials_tensor, weights_tensor = get_dna_leaves_partials_compressed(dna)
-    mats = jc69_model.p_t(branch_lengths_tensor)
-    log_p = likelihood.calculate_treelikelihood(partials_tensor, weights_tensor, indices, mats, jc69_model.frequencies)
+    return partials_tensor, weights_tensor, indices, branch_lengths
+
+
+def test_calculate_pytorch(tiny_newick_file, tiny_fasta_file, jc69_model):
+    partials_tensor, weights_tensor, indices, branch_lengths = _prepare_tiny(
+        tiny_newick_file, tiny_fasta_file
+    )
+    mats = jc69_model.p_t(branch_lengths)
+
+    log_p = likelihood.calculate_treelikelihood(
+        partials_tensor, weights_tensor, indices, mats, jc69_model.frequencies
+    )
     assert torch.allclose(torch.tensor(-83.329016, dtype=log_p.dtype), log_p)
+
+
+def test_calculate_pytorch_rescaled(tiny_newick_file, tiny_fasta_file, jc69_model):
+    partials_tensor, weights_tensor, indices, branch_lengths = _prepare_tiny(
+        tiny_newick_file, tiny_fasta_file
+    )
+    mats = jc69_model.p_t(branch_lengths.reshape((-1, 1)))
+    freqs = jc69_model.frequencies.reshape(jc69_model.frequencies.shape[:-1] + (1, -1))
+    props = torch.tensor([[[1.0]]])
+
+    log_p = likelihood.calculate_treelikelihood_discrete(
+        partials_tensor, weights_tensor, indices, mats, freqs, props
+    )
+
+    log_p_rescaled = likelihood.calculate_treelikelihood_discrete_rescaled(
+        partials_tensor, weights_tensor, indices, mats, freqs, props
+    )
+    assert torch.allclose(log_p_rescaled, log_p)
 
 
 def test_treelikelihood_json(tiny_newick_file, tiny_fasta_file):
@@ -34,16 +71,13 @@ def test_treelikelihood_json(tiny_newick_file, tiny_fasta_file):
             "id": "alignment",
             "type": "phylotorch.evolution.alignment.Alignment",
             'file': tiny_fasta_file,
-            'taxa': 'taxa'
-        }
+            'taxa': 'taxa',
+        },
     }
-    subst_model = {
-        'id': 'm',
-        'type': 'phylotorch.evolution.substitution_model.JC69'
-    }
+    subst_model = {'id': 'm', 'type': 'phylotorch.evolution.substitution_model.JC69'}
     site_model = {
         'id': 'sm',
-        'type': 'phylotorch.evolution.site_model.ConstantSiteModel'
+        'type': 'phylotorch.evolution.site_model.ConstantSiteModel',
     }
     tree_model = {
         'id': 'tree',
@@ -54,19 +88,58 @@ def test_treelikelihood_json(tiny_newick_file, tiny_fasta_file):
             'id': 'taxa',
             'type': 'phylotorch.evolution.taxa.Taxa',
             'taxa': [
-                {"id": "A_Belgium_2_1981", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1981}},
-                {"id": "A_ChristHospital_231_1982", "type": "phylotorch.evolution.taxa.Taxon",
-                 "attributes": {"date": 1982}},
-                {"id": "A_Philippines_2_1982", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1982}},
-                {"id": "A_Baylor1B_1983", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1983}},
-                {"id": "A_Oita_3_1983", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1983}},
-                {"id": "A_Texas_12764_1983", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1983}},
-                {"id": "A_Alaska_8_1984", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1984}},
-                {"id": "A_Caen_1_1984", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1984}},
-                {"id": "A_Texas_17988_1984", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1984}},
-                {"id": "A_Colorado_2_1987", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1987}}
-            ]
-        }
+                {
+                    "id": "A_Belgium_2_1981",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1981},
+                },
+                {
+                    "id": "A_ChristHospital_231_1982",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1982},
+                },
+                {
+                    "id": "A_Philippines_2_1982",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1982},
+                },
+                {
+                    "id": "A_Baylor1B_1983",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1983},
+                },
+                {
+                    "id": "A_Oita_3_1983",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1983},
+                },
+                {
+                    "id": "A_Texas_12764_1983",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1983},
+                },
+                {
+                    "id": "A_Alaska_8_1984",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1984},
+                },
+                {
+                    "id": "A_Caen_1_1984",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1984},
+                },
+                {
+                    "id": "A_Texas_17988_1984",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1984},
+                },
+                {
+                    "id": "A_Colorado_2_1987",
+                    "type": "phylotorch.evolution.taxa.Taxon",
+                    "attributes": {"date": 1987},
+                },
+            ],
+        },
     }
     tree_likelihood = {
         'id': 'like',
@@ -74,7 +147,7 @@ def test_treelikelihood_json(tiny_newick_file, tiny_fasta_file):
         'tree_model': tree_model,
         'site_model': site_model,
         'site_pattern': site_pattern,
-        'substitution_model': subst_model
+        'substitution_model': subst_model,
     }
 
     like = likelihood.TreeLikelihoodModel.from_json(tree_likelihood, {})
@@ -86,11 +159,27 @@ def test_treelikelihood_batch():
         'id': 'taxa',
         'type': 'phylotorch.evolution.taxa.Taxa',
         'taxa': [
-            {"id": "A", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 0.0}},
-            {"id": "B", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 1.0}},
-            {"id": "C", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 4.0}},
-            {"id": "D", "type": "phylotorch.evolution.taxa.Taxon", "attributes": {"date": 5.0}}
-        ]
+            {
+                "id": "A",
+                "type": "phylotorch.evolution.taxa.Taxon",
+                "attributes": {"date": 0.0},
+            },
+            {
+                "id": "B",
+                "type": "phylotorch.evolution.taxa.Taxon",
+                "attributes": {"date": 1.0},
+            },
+            {
+                "id": "C",
+                "type": "phylotorch.evolution.taxa.Taxon",
+                "attributes": {"date": 4.0},
+            },
+            {
+                "id": "D",
+                "type": "phylotorch.evolution.taxa.Taxon",
+                "attributes": {"date": 5.0},
+            },
+        ],
     }
     tree_model_dict = {
         'id': 'tree',
@@ -99,9 +188,9 @@ def test_treelikelihood_batch():
         'node_heights': {
             'id': 'heights',
             'type': 'phylotorch.Parameter',
-            'tensor': [[10., 20., 30.], [100., 200., 300.]]
+            'tensor': [[10.0, 20.0, 30.0], [100.0, 200.0, 300.0]],
         },
-        'taxa': taxa_dict
+        'taxa': taxa_dict,
     }
     tree_model_dict2 = {
         'id': 'tree',
@@ -110,9 +199,9 @@ def test_treelikelihood_batch():
         'node_heights': {
             'id': 'heights',
             'type': 'phylotorch.Parameter',
-            'tensor': [100., 200., 300.]
+            'tensor': [100.0, 200.0, 300.0],
         },
-        'taxa': taxa_dict
+        'taxa': taxa_dict,
     }
     site_pattern_dict = {
         "id": "sites",
@@ -127,22 +216,33 @@ def test_treelikelihood_batch():
                 {"taxon": "B", "sequence": "AAC"},
                 {"taxon": "C", "sequence": "AAC"},
                 {"taxon": "D", "sequence": "AAT"},
-            ]
-        }
+            ],
+        },
     }
     subst_model = JC69('jc')
     # compute using a batch of 2 samples
     dic = {}
     tree_model = TimeTreeModel.from_json(tree_model_dict, dic)
     site_pattern = SitePattern.from_json(site_pattern_dict, dic)
-    site_model = WeibullSiteModel(None, Parameter(None, torch.tensor([[1.], [1.]])), 4)
-    clock_model = StrictClockModel(None, Parameter(None, torch.tensor([[0.01], [.001]])), tree_model)
-    like_batch = likelihood.TreeLikelihoodModel(None, site_pattern, tree_model, subst_model, site_model, clock_model)
+    site_model = WeibullSiteModel(
+        None, Parameter(None, torch.tensor([[1.0], [1.0]])), 4
+    )
+    clock_model = StrictClockModel(
+        None, Parameter(None, torch.tensor([[0.01], [0.001]])), tree_model
+    )
+    like_batch = likelihood.TreeLikelihoodModel(
+        None, site_pattern, tree_model, subst_model, site_model, clock_model
+    )
 
-    # compute using a batch of 1 sample (the second sample from the previous computation)
+    # compute using a batch of 1 sample
+    # (the second sample from the previous computation)
     tree_model2 = TimeTreeModel.from_json(tree_model_dict2, {})
-    site_model2 = WeibullSiteModel(None, Parameter(None, torch.tensor([1.])), 4)
-    clock_model2 = StrictClockModel(None, Parameter(None, torch.tensor([.001])), tree_model2)
-    like = likelihood.TreeLikelihoodModel(None, site_pattern, tree_model2, subst_model, site_model2, clock_model2)
+    site_model2 = WeibullSiteModel(None, Parameter(None, torch.tensor([1.0])), 4)
+    clock_model2 = StrictClockModel(
+        None, Parameter(None, torch.tensor([0.001])), tree_model2
+    )
+    like = likelihood.TreeLikelihoodModel(
+        None, site_pattern, tree_model2, subst_model, site_model2, clock_model2
+    )
 
     assert like() == like_batch()[1]
